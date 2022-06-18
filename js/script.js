56745a36ba51ee599e42c248b48093d1e986cca4
thekashfi <alikashfi12345@gmail.com>
2022-06-18 12:42:11 +0430

    read contacts from api

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(() => {
+    const Item = (result) => `
+        <div class="col-lg-6">
+            <div class="card m-b-30">
+                <div class="card-body py-5">
+                    <div class="row">
+                        <div class="col-lg-3 text-center">
+                            <img src="${result.picture.large}" class="img-fluid mb-3" alt="user" />
+                        </div>
+                        <div class="col-lg-9">
+                            <h4>${result.name.title}. ${result.name.first} ${result.name.last}</h4>
+                            <p>${result.phone}</p>
+                            <div class="button-list mt-4 mb-3">
+                                <button type="button" class="btn btn-primary-rgba"><i class="feather mr-1" data-feather="message-square"></i>Message</button>
+                                <button type="button" class="btn btn-success-rgba"><i class="feather mr-1" data-feather="phone"></i>Call Now</button>
+                                <button type="button" class="btn btn-light" onclick="
+                                        if(confirm (\`آیا از حذف ${result.name.title}. ${result.name.first} ${result.name.last} مطمئن هستید؟\`)) {
+                                            document.querySelector('#deleteForm_1').submit();
+                                        }
+                                    "><i class="feather" data-feather="trash-2"></i></button>
+                                <form action="delete/1" method="post" id="deleteForm_1" hidden></form>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table table-borderless mb-0">
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row" class="p-1">Email :</th>
+                                        <td class="p-1">${result.email}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="p-1">Gender :</th>
+                                        <td class="p-1">${result.gender}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="p-1">Country :</th>
+                                        <td class="p-1">${result.location.country}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="p-1">City :</th>
+                                        <td class="p-1">${result.location.city}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    async function fetch() {
+        return await $.get("https://www.randomuser.me/api/?seed=erfan&results=10&page=1", function(data, status){
+            // alert("Data: " + data + "\nStatus: " + status);
+            $('#contacts-container').html(
+                data.results
+            .map(Item).join(''));
+        });
+    }
+
+    fetch()
+        .then(() => {
+            feather.replace()
+            $('#spinner').hide()
+        })
+})
